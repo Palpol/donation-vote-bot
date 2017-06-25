f2421447c78ed40fa39099245b3262631b627008
thrize <thrize@tutanota.de>
2017-06-25 14:32:02 +0100

    Add first part of transaction validity test

--- bot.js
+++ bot.js
@@ -4,7 +4,8 @@ const
   steem = require("steem"),
   path = require("path"),
   mongodb = require("mongodb"),
-  moment = require('moment');
+  moment = require('moment'),
+  S = require('string');
 
 const
   DB_RECORDS = "records";
@@ -29,47 +30,79 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
 function main() {
   console.log("donation-vote-bot waking up");
   steem.api.setWebSocket('wss://steemd.steemit.com');
-  getLastTimeAsEpoch(function (lastTransactionTimeAsEpoch) {
+  getLastInfos(function (lastTransactionTimeAsEpoch, lastTransactionNumber) {
     steem.api.getAccountHistory(process.env.STEEM_USER, 10, 10, function(err, result) {
-      console.log("*** ACCOUNT HISTORY");
       if (err) {
-        console.log("error!");
+        console.log("fatal error, cannot get account history" +
+          " (transactions)");
       } else {
         console.log(JSON.stringify(result));
         for (var j = 0 ; j < result.length ; j++) {
           var r = result[j];
-          //console.log(" - entry: "+JSON.stringify(r));
           console.log(" - entry");
           if (r !== undefined && r !== null && r.length > 1) {
             var transaction = r[1];
             //console.log(" - - transaction: "+JSON.stringify(transaction));
             console.log(" - - transaction");
             for (var i = 0 ; i < transaction.op.length ; i += 2) {
-              console.log(" - - - "+transaction.op[i]);
-              console.log(" - - - - detail: "+JSON.stringify(transaction.op[i+1]));
+              var opName = transaction.op[i];
+              console.log(" - - - "+opName);
+              if (opName.localeCompare("transaction") == 0) {
+                var opDetail = transaction.op[i+1];
+                verifyTransferIsValid(opDetail, function (err) {
+                  if (err) {
+                    console.log("verifyTransferIsValid failed: "+err);
+                  } else {
+                    console.log("verifyTransferIsValid pass!");
+                  }
+                });
+              }
             }
           }
         }
       }
-      /*
-      if (result !== undefined && result !== null && result.length > 0) {
-        for (var r in result) {
-          steem.api.getTransaction(r[1].trx_id, function(err, result) {
-            if (err) {
-              console.log("error getting transaction "+r[1].trx_id);
-            } else {
-              console.log("transaction: "+r[1].trx_id);
-              console.log(result);
-            }
-          });
-        }
-      }
-      */
     });
   });
 }
 
-function getLastTimeAsEpoch(callback) {
+function verifyTransferIsValid(opDetail, callback) {
+  console.log(" - - - - detail: "+JSON.stringify(opDetail));
+  // CHECK 1: only consider STEEM transactions, not SBD
+  if (opDetail.asset.localeCompare("STEEM") == 0) {
+    console.log(" - - - - MATCH, is for STEEM");
+    if (opDetail.amount >= 1.0) {
+      console.log(" - - - - MATCH, amount >= 1.0");
+      var parts = opDetail.memo.split("/");
+      if (parts.length > 0) {
+        var permlink = parts[parts.length - 1];
+        for (var i = 0 ; i < parts.length ; i++) {
+          if (S(parts[i]).startsWith("@")) {
+            var author = parts[i].substr(1, parts[i].length);
+            // check exists by fetching from Steem API
+            steem.api.getContent(author, permlink, function(err, result) {
+              if (err) {
+                callback("Transfer memo does not contain valid post URL" +
+                  " (failed at fetch author/permlink content from API)");
+              } else {
+                console.log("DEBUG get post content: "+JSON.stringify(result));
+                callback(null);
+              }
+            });
+          }
+        }
+      } else {
+        callback("Transfer memo does not contain valid post URL (failed" +
+          " at URL split by /)");
+      }
+    } else {
+      callback("Transfer amount < 1.0 STEEM");
+    }
+  } else {
+    callback("Transfer is not for STEEM");
+  }
+}
+
+function getLastInfos(callback) {
   db.collection(DB_RECORDS).find({}).toArray(function(err, data) {
     if (err) {
       console.log(err);
@@ -77,6 +110,7 @@ function getLastTimeAsEpoch(callback) {
       callback(0);
       return;
     }
+    var lastTransactionNumber = -1;
     var lastTransactionTimeAsEpoch = 0;
     if (process.env.START_TIME_AS_EPOCH !== undefined
       && process.env.START_TIME_AS_EPOCH !== null) {
@@ -94,11 +128,12 @@ function getLastTimeAsEpoch(callback) {
         if (lastTransactionTimeAsEpoch < data[0].timeAsEpoch) {
           lastTransactionTimeAsEpoch = data[0].timeAsEpoch;
         }
+        lastTransactionNumber = data[0].trxNumber;
       } catch(err) {
         console.log(err);
         console.log("not fatal, continuing");
       }
     }
-    callback(lastTransactionTimeAsEpoch);
+    callback(lastTransactionTimeAsEpoch, lastTransactionNumber);
   });
 }
